062835d40248b7f6e4e55a5e71c497332cb87e0f
Diana Kariuki <dianakariuki842@gmail.com>
2022-03-12 01:47:56 +0300

    Added the product and development class jquery syntax

--- index.js
+++ index.js
@@ -3,4 +3,31 @@ $(document).ready(function () {
         $(".design").slideDown('1500').hide('1000');
         $(".design-one").show('1500');
       });
+
+      $(".design-one").click(function(){
+        $(".design-one").slideUp('1500');
+        $(".design").slideDown('1500');
+      });
+//design icon js
+      $(".product").click(function(){
+        $(".product").slideDown('1500').hide('1000');
+        $(".product-one").show('1500');
+      });
+
+      $(".product-one").click(function(){
+        $(".product-one").slideUp('1500');
+        $(".product").slideDown('1500');
+      });
+      //product icon js 
+
+      $(".development").click(function(){
+        $(".development").slideDown('1500').hide('1000');
+        $(".dev-one").show('1500');
+      });
+
+      $(".dev-one").click(function(){
+        $(".dev-one").slideUp('1500');
+        $(".development").slideDown('1500');
+     });
+     //dev icon js
 })

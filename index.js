0b17f5fdcf7e58d5762d5a2281f44a25768ccf52
Diana Kariuki <dianakariuki842@gmail.com>
2022-03-12 01:39:52 +0300

    updates styling

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-    
-}
+    $(".design").click(function(){
+        $(".design").slideDown('1500').hide('1000');
+        $(".design-one").show('1500');
+      });
+})
